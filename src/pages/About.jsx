1f995c396a5c3e92565d6dc54ecaeb3b182cbb63
ZHIE27 <rivalfahrezi27@gmail.com>
2025-03-17 20:39:35 +0700

    add about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,49 @@
- const About = ()=>{
-    return (
-        <h1>ABOUT</h1>
-    )
- }
+import data from "../components/data/data.json";
 
- export default About;
+const About = () => {
+  return (
+    <div>
+      {/* TOP CONTENT */}
+      <div className="w-full bg-[#205781] text-center flex items-center min-h-screen px-4">
+        <div className="wrap w-full flex flex-col items-center h-40">
+          <p className="text-white text-4xl md:text-6xl font-bold">{data.aboutPage.title[0]}</p>
+          <div className="flex flex-wrap justify-center">
+            <p className="text-white text-4xl md:text-6xl font-bold">{data.aboutPage.title[1]}</p>
+            <p className="text-cyan-400 text-4xl md:text-6xl font-bold ms-3 md:ms-7">
+              {data.aboutPage.title[2]}
+            </p>
+          </div>
+        </div>
+        <div className="hidden lg:flex lg:w-full items-center min-h-screen">
+          <div className="w-xl min-h-40 h-auto p-6 flex items-center rounded-md mx-4 bg-amber-50">
+            <p className="italic">{data.aboutPage.desc}</p>
+          </div>
+        </div>
+      </div>
+
+      {/* OUR STORY */}
+      <div className="w-full flex flex-col-reverse md:flex-row p-7 justify-between items-center bg-slate-100 min-h-screen">
+        <div className="text-center shadow-lg w-full md:w-xl p-6 rounded-md">
+          <h1 className="font-bold text-4xl md:text-6xl mb-4">{data.aboutPage.ourStory}</h1>
+          <p>{data.aboutPage.descStory}</p>
+        </div>
+        <div className="w-full md:w-xl flex items-center justify-center h-80 md:h-screen">
+          <img className="w-60 md:w-80 h-60 md:h-80" src={data.aboutPage.icon1} alt="woman" />
+        </div>
+      </div>
+
+      {/* OUR VISION */}
+      <div className="w-full flex flex-col md:flex-row p-7 justify-between items-center bg-slate-100 min-h-screen">
+        <div className="w-full md:w-xl flex items-center justify-center h-80 md:h-screen">
+          <img className="w-60 md:w-80 h-60 md:h-80" src={data.aboutPage.icon2} alt="vision" />
+        </div>
+        <div className="text-center shadow-lg w-full md:w-xl p-6 lg:min-h-[350px] rounded-md">
+          <h1 className="font-bold text-4xl md:text-6xl mb-4">{data.aboutPage.ourVision}</h1>
+          <p>{data.aboutPage.descVision}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
