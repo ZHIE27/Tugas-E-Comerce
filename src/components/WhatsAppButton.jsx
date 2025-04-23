92486b8642a9d6eabe06c4cdd143ae2704653996
ZHIE27 <rivalfahrezi27@gmail.com>
2025-04-23 20:14:56 +0700

    update

--- src/components/WhatsAppButton.jsx
+++ src/components/WhatsAppButton.jsx
@@ -1,4 +1,6 @@
 import { FaWhatsapp } from 'react-icons/fa';
+import { useState, useEffect } from 'react';
+import { useLocation } from "react-router-dom";
 import '../App.css';
 
 const WhatsAppButton = ({
@@ -6,16 +8,29 @@ const WhatsAppButton = ({
   message = "Halo, saya tertarik untuk menggunakan jasa travel",
   label = ""
 }) => {
+
+  const [showButton, setShowButton] = useState(true);
+  const location = useLocation();
+
+  useEffect(() => {
+    const visiblePaths = ["/home", "/about", "/contact", "/pricing"];
+    setShowButton(visiblePaths.includes(location.pathname));
+  }, [location]);
+
   const handleClick = () => {
     const encodedMessage = encodeURIComponent(message);
     window.open(`https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodedMessage}`, '_blank');
   };
 
   return (
-    <div className="whatsapp-button" onClick={handleClick}>
-      <FaWhatsapp className="whatsapp-icon" />
-      {label && <span className="whatsapp-label">{label}</span>}
-    </div>
+    <>
+      {showButton && (
+        <button className="whatsapp-button" onClick={handleClick}>
+          <FaWhatsapp className="whatsapp-icon" />
+          {label && <span className="whatsapp-label">{label}</span>}
+        </button>
+      )}
+    </>
   );
 };
 
